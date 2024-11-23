776f9b8ecae6a2370cce293b905a723f65174997
Pranav-Patani <pranav.patani@gmail.com>
2024-11-23 21:50:43 +0530

    feat: changed font and images

--- src/card.jsx
+++ src/card.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./birthday-card.css";
+import drum from "./images/drum.webp";
 
-const images = [
-  "https://images.unsplash.com/photo-1623244727304-54995b233b1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1623244727304-54995b233b1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1623244727304-54995b233b1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1623244727304-54995b233b1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-];
+const images = [`${drum}`, `${drum}`, `${drum}`, `${drum}`];
 
 export default function BirthdayCard() {
   const [isFlipped, setIsFlipped] = useState(false);
